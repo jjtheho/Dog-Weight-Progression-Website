eded2972d765b2fcf0209bf164c3f3278a90408d
Jaeho Jung <jaju2017@v1040-wn-rt-c-70-13.campus-dynamic.uwaterloo.ca>
2019-03-08 11:46:10 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+class Item {
+    constructor(name, age, weight){
+        this.type = name;
+        this.age = age;
+        this.weight = weight;
+    }
+}
+
+var itemOne = new Item('Water', 6, 4.5);
+
+
+
